14e1fe8da55978dca9ffc63c67ff7b2db38843fc
Viguier <leo.vgr@hotmail.com>
2018-05-02 12:54:01 +0200

    Maj

---
+++ src/ihm/AffichageNomGrille.java
@@ -0,0 +1,12 @@
+package ihm;
+
+import javax.swing.JLabel;
+
+public class AffichageNomGrille extends JLabel{
+	public AffichageNomGrille() {
+		super();
+	}
+	public AffichageNomGrille(String nom) {
+		super(nom);
+	}
+}
